4fbf0015c9311defbae548f9ba5a7035a8ccaf44
yusuf <y.a.rouf2@newcastle.ac.uk>
2025-11-02 02:02:20 +0000

    allow for guest users

--- backend/src/firebase_client.py
+++ backend/src/firebase_client.py
@@ -1,28 +1,30 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1 import SERVER_TIMESTAMP
 
 cred = credentials.Certificate("serviceAccountKey.json")
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def add_notes(author, color, text, board="default_board"):
+def add_notes(author, color, text, guest_id, board="default_board"):
     note_ref = db.collection("notes").document()
     note = {
         "author": author,
         "color": color,
-        "text": text
+        "text": text,
+        "guest_id": guest_id,
+        "board": board,
+        "created_at": SERVER_TIMESTAMP,
+        "updated_at": SERVER_TIMESTAMP
     }
     note_ref.set(note)
     return note_ref.id
 
 def get_notes(board="default_board"):
-    notes_ref = db.collection("notes")
+    notes_ref = db.collection("notes").where("board", "==", board).order_by("created_at", direction=firestore.Query.DESCENDING)
     docs = notes_ref.stream()
-    notes = []
-    for doc in docs:
-        notes.append(doc.to_dict())
-    return notes
+    return [doc.to_dict() for doc in docs]
 
 def delete_note(note_id, board="default_board"):
     notes_ref = db.collection("notes").where("board", "==", board)
@@ -33,4 +35,11 @@ def delete_note(note_id, board="default_board"):
 
     return False
 
+def edit_node(note_id, new_text):
+    note_ref = db.collection("notes").document(note_id)
+    note_ref.update({
+        "text": new_text,
+        "updated_at": SERVER_TIMESTAMP
+    })
+    return True
 
